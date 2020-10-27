af5ecbf2bba24884ac5255106d53de04e8b785b6
emixam16 <maxime.belair@imt-atlantique.fr>
2020-10-27 10:58:16 +0100

    Add 2 more functions

--- double_free.c
+++ double_free.c
@@ -1,4 +1,27 @@
 #include "header.h"
+
+int invalid_array(int arg) {
+	int *a = malloc(arg);
+	free(a);
+	return 0;
+}
+void invalid_array_usage() {
+	printf("invalid array -- Allocate an array, invalid if arg < 0\n");
+}
+
+
+int memory_leak(int arg) {
+	int*x;
+	if(arg > 0)
+		x =  malloc(1);
+	return 0;
+}
+void memory_leak_usage() {
+	printf("dont free -- Memory leak if arg > 0\n");
+}
+
+
+
 int double_free(int arg) {
 	int* x =  malloc(1);
 	if(arg > 0) {
@@ -10,6 +33,8 @@ int double_free(int arg) {
 void double_free_usage() {
 	printf("double free -- crashes if arg > 0\n");
 }
+
+
 
 int invalid_read(int arg) {
 	int* x =  malloc(1);
--- functions.h
+++ functions.h
@@ -1,2 +1,4 @@
 FN(double_free)
 FN(invalid_read)
+FN(memory_leak)
+FN(invalid_array)
